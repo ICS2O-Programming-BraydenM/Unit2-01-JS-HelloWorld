588436e306c13d07ad281fb4b8dc2796b5de4a43
Brayden MacMillan <99274619+BraydenMacMillan7@users.noreply.github.com>
2022-03-11 23:26:31 +0000

    updated everything

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Ms Raffin All rights reserved
+//
+// Created by: Brayden MacMillan
+// Created on: Mar 11 2022
+// This file contains the JS functions for index.html
+
+function myButtonClicked () {
+  // this function displays "Hello, World!"
+  
+  document.getElementById('hello-world').innerHTML = 'I coded this in JavaScript! It is still confusing but I am learning new things about it by the minute.'
+}
